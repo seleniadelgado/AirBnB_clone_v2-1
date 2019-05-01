b176bb6efade6ff4277fed01463535ea4c67105e
jozsa <allisonjoyweiner@gmail.com>
2019-05-01 21:13:21 +0000

    Add /stats route to retrieve number of each object by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,20 @@
 from api.v1.views import app_views
 from flask import jsonify
 import json
+from models import storage
 
+stat_dict = {"amenities": storage.count("amenities"),
+             "cities": storage.count("cities"),
+             "places": storage.count("places"),
+             "reviews": storage.count("reviews"),
+             "states": storage.count("states"),
+             "users": storage.count("users")}
 
 @app_views.route('/status', strict_slashes=False)
 def status_route():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats_route():
+    return jsonify(stat_dict)
+
