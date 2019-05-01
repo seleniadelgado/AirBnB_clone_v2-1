027c494af50c30021b1b1ee9b2bb47bd7b7bd697
jozsa <allisonjoyweiner@gmail.com>
2019-05-01 20:55:16 +0000

    Use jsonify method of flask instead of Response

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,10 @@
 
 """
 from api.v1.views import app_views
-from flask import Response
+from flask import jsonify
 import json
 
 
 @app_views.route('/status', strict_slashes=False)
 def status_route():
-    return Response(json.dumps({"status": "OK"}), mimetype='application/json')
+    return jsonify("status": "OK"})
