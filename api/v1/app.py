434c7af6788537a405b14c6777fc80860bcc622d
--get <angiedelgado08@gmail.com>
2019-05-01 23:46:24 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask import Flask
 from flask import Blueprint
 from models import storage
 from api.v1.views import app_views
-
+from flask import jsonify
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -19,6 +19,12 @@ def _close(self):
     """closes app"""
     storage.close()
 
+@app.errorhandler(404)
+def  page_not_found(e):
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
+    
 if __name__ == "__main__":
     if "HBNB_API_HOST" in os.environ:
         hostip = os.environ["HBNB_API_HOST"]
@@ -28,4 +34,4 @@ if __name__ == "__main__":
         portnbr = os.environ["HBNB_API_PORT"]
     else:
         portnbr = 5000
-    app.run(hostip, port=portnbr, threaded=True)
+    app.run(host=hostip, port=portnbr, threaded=True)
