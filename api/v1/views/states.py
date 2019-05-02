1d72f3d402d0223159e2b058a0f0271195bdd685
jozsa <allisonjoyweiner@gmail.com>
2019-05-02 22:17:35 +0000

    Return JSON formatted error message

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,7 @@ Handles RESTful API actions for State objects
 from models import storage
 from models.state import State
 from api.v1.views import app_views
-from flask import request, jsonify, abort, app
+from flask import request, jsonify, abort
 
 
 @app_views.route('/states', strict_slashes=False, methods=['GET', 'POST'])
@@ -17,9 +17,9 @@ def state_route():
     if request.method == 'POST':
         req_dict = request.get_json(silent=True)
         if req_dict is None:
-            return 'Not a JSON', 400
+            return jsonify('Not a JSON'), 400
         if 'name' not in req_dict.keys():
-            return 'Missing name', 400
+            return jsonify('Missing name'), 400
         new = State(**req_dict)
         new.save()
         return jsonify(new.to_dict()), 201
@@ -40,7 +40,7 @@ def state_id_route(state_id):
     if request.method == 'PUT':
         req_dict = request.get_json(silent=True)
         if req_dict is None:
-            abort(400, 'Not a JSON')
+            return jsonify('Not a JSON'), 400
         for key, value in req_dict.items():
             if key != 'id' or key != 'created_at' or key != 'updated_at':
                 setattr(state, key, value)
