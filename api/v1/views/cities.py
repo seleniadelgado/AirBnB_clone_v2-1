d9bad6adcb881f55342d35bb3dbedac37ded1d70
jozsa <allisonjoyweiner@gmail.com>
2019-05-03 04:08:33 +0000

    Removed unnecessary (/cities) route and moved into appropriate route (/states/state_id/cities)

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,22 +8,6 @@ from api.v1.views import app_views
 from flask import request, jsonify, abort
 
 
-@app_views.route('/cities', strict_slashes=False, methods=['GET', 'POST'])
-def city_route():
-    if request.method == 'GET':
-        city_list = [city.to_dict() for city in storage.all("City").values()]
-        return jsonify(city_list)
-        if request.method == 'POST':
-            req_dict = request.get_json(silent=True)
-            if req_dict is None:
-                return jsonify({"error": "Not a JSON"}), 400
-            if 'name' not in req_dict.keys():
-                return jsonify({"error": "Missing name"}), 400
-            new = city(**req_dict)
-            new.save()
-            return jsonify(new.to_dict()), 201
-
-
 @app_views.route('/cities/<city_id>', strict_slashes=False,
                  methods=['GET', 'DELETE', 'PUT'])
 def city_id_route(city_id):
@@ -58,3 +42,12 @@ def city_state_id_route(state_id):
         city_list = [city.to_dict() for city in storage.all("City").values()
                      if city.state_id == state_id]
         return jsonify(city_list)
+    if request.method == 'POST':
+        req_dict = request.get_json(silent=True)
+        if req_dict is None:
+            return jsonify({"error": "Not a JSON"}), 400
+        if 'name' not in req_dict.keys():
+            return jsonify({"error": "Missing name"}), 400
+        new = city(**req_dict)
+        new.save()
+        return jsonify(new.to_dict()), 201
